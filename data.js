a6b386087854d93d6e9867b2beee2dc4f879088d
LAGA <gutierrezanicamaluisalberto@hotmail.com>
2020-10-27 17:31:46 -0500

    Estilos y Data generados automáticamente desde la data de JS

--- data.js
+++ data.js
@@ -1,32 +1,23 @@
+
 export const data = [
     {
-        imageDesktop: "https://placehold.it/250X250",
-        imageMobile: "https://placehold.it/250X250",
-        title: "Título oficial del proyecto de FrontEnd Mentor Challenges",
-        description: "Descripción oficial del proyecto de FrontEnd Mentor Challenges modificado desde mi perspectiva y traducido al castellano",
-        nivel: 0,
-        url: "https://lagaxyz.github.io/FrontEnd-Mentor-Challenges/",
-        code: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges",
-        tools: ["HTML", "CSS","FlexBox", "Grid", "less", "Sass", "JS", "JQuery", "Bootstrap", "API"]
+        imageDesktop: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges/blob/master/01%20-%20fylo%20data%20storage%20component%20master/design/desktop-design.jpg?raw=true",
+        imageMobile: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges/blob/master/01%20-%20fylo%20data%20storage%20component%20master/design/mobile-design.jpg?raw=true",
+        title: "Fylo data storage component",
+        description: "Este reto tiene algunos desafíos CSS muy interesantes en su diseño que pusieron en práctica mis habilidades y conocimientos en el tema",
+        nivel: 1,
+        url: "./01 - fylo data storage component master/index.html",
+        code: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges/tree/master/01%20-%20fylo%20data%20storage%20component%20master",
+        tools: ["HTML", "CSS","FlexBox"]
     },
     {
-        imageDesktop: "https://placehold.it/250X250",
-        imageMobile: "https://placehold.it/250X250",
-        title: "Título oficial del proyecto de FrontEnd Mentor Challenges",
-        description: "Descripción oficial del proyecto de FrontEnd Mentor Challenges modificado desde mi perspectiva y traducido al castellano",
-        nivel: 0,
-        url: "https://lagaxyz.github.io/FrontEnd-Mentor-Challenges/",
-        code: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges",
-        tools: ["HTML", "CSS","FlexBox", "Grid", "less", "Sass", "JS", "JQuery", "Bootstrap", "API"]
-    },
-    {
-        imageDesktop: "https://placehold.it/250X250",
-        imageMobile: "https://placehold.it/250X250",
-        title: "Título oficial del proyecto de FrontEnd Mentor Challenges",
-        description: "Descripción oficial del proyecto de FrontEnd Mentor Challenges modificado desde mi perspectiva y traducido al castellano",
-        nivel: 0,
-        url: "https://lagaxyz.github.io/FrontEnd-Mentor-Challenges/",
-        code: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges",
-        tools: ["HTML", "CSS","FlexBox", "Grid", "less", "Sass", "JS", "JQuery", "Bootstrap", "API"]
+        imageDesktop: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges/blob/master/02%20-%20social%20proof%20section%20master/design/desktop-design.jpg?raw=true",
+        imageMobile: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges/blob/master/02%20-%20social%20proof%20section%20master/design/mobile-design.jpg?raw=true",
+        title: "Social proof section",
+        description: "Este reto estuvo perfecto para practicar y mejorar mis habilidades en FlexBox y Web Responsive Design",
+        nivel: 1,
+        url: "./02 - social proof section master/index.html",
+        code: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges/tree/master/02%20-%20social%20proof%20section%20master",
+        tools: ["HTML", "CSS","FlexBox"]
     }
 ];
--- main.js
+++ main.js
@@ -1,1 +1,45 @@
 import {data} from "./data.js";
+
+const mostrarData = ()=>{
+    const contenedor = document.getElementById("contenedor");
+    contenedor.innerHTML = "";
+
+    data.forEach((dato)=>{
+        let col = document.createElement("div");
+        let card = document.createElement("div");
+        let img = document.createElement("img");
+        let body = document.createElement("div");
+        let titulo = document.createElement("h5");
+        let hr = document.createElement("hr");
+        let descripcion = document.createElement("p");
+        let vinculo = document.createElement("a");
+    
+        col.classList.add("col-lg-4", "col-md-6","col-sm-12");
+        card.classList.add("card", "shadow", "text-center", "m-2");
+        img.classList.add("card-img-top");
+        body.classList.add("card-body");
+        titulo.classList.add("card-title");
+        descripcion.classList.add("card-text");
+        vinculo.classList.add("btn", "btn-dark");
+    
+        img.src = dato.imageDesktop;
+        titulo.innerText = dato.title;
+        descripcion.innerText = dato.description;
+        vinculo.innerText = "Ver Sitio"
+        vinculo.href = dato.url;
+    
+        body.appendChild(titulo);
+        body.appendChild(hr);
+        body.appendChild(descripcion);
+        body.appendChild(vinculo);
+    
+        card.appendChild(img);
+        card.appendChild(body);
+    
+        col.appendChild(card);
+    
+        contenedor.appendChild(col);
+    });
+};
+
+mostrarData();
