4258085dc99d4c33d3c39b8bb8861ff2ae6aeaff
LAGA <gutierrezanicamaluisalberto@hotmail.com>
2020-10-26 22:53:40 -0500

    Data inicial agregada

---
+++ data.js
@@ -0,0 +1,32 @@
+const data = [
+    {
+        imageDesktop: "https://placehold.it/250X250",
+        imageMobile: "https://placehold.it/250X250",
+        title: "Título oficial del proyecto de FrontEnd Mentor Challenges",
+        description: "Descripción oficial del proyecto de FrontEnd Mentor Challenges modificado desde mi perspectiva y traducido al castellano",
+        nivel: 0,
+        url: "https://lagaxyz.github.io/FrontEnd-Mentor-Challenges/",
+        code: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges",
+        tools: ["HTML", "CSS","FlexBox", "Grid", "less", "Sass", "JS", "JQuery", "Bootstrap", "API"]
+    },
+    {
+        imageDesktop: "https://placehold.it/250X250",
+        imageMobile: "https://placehold.it/250X250",
+        title: "Título oficial del proyecto de FrontEnd Mentor Challenges",
+        description: "Descripción oficial del proyecto de FrontEnd Mentor Challenges modificado desde mi perspectiva y traducido al castellano",
+        nivel: 0,
+        url: "https://lagaxyz.github.io/FrontEnd-Mentor-Challenges/",
+        code: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges",
+        tools: ["HTML", "CSS","FlexBox", "Grid", "less", "Sass", "JS", "JQuery", "Bootstrap", "API"]
+    },
+    {
+        imageDesktop: "https://placehold.it/250X250",
+        imageMobile: "https://placehold.it/250X250",
+        title: "Título oficial del proyecto de FrontEnd Mentor Challenges",
+        description: "Descripción oficial del proyecto de FrontEnd Mentor Challenges modificado desde mi perspectiva y traducido al castellano",
+        nivel: 0,
+        url: "https://lagaxyz.github.io/FrontEnd-Mentor-Challenges/",
+        code: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges",
+        tools: ["HTML", "CSS","FlexBox", "Grid", "less", "Sass", "JS", "JQuery", "Bootstrap", "API"]
+    }
+];
