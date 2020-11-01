35968376d390129a94b32f79d89103fb35c17549
LAGA <gutierrezanicamaluisalberto@hotmail.com>
2020-11-01 18:44:13 -0500

    data actualizada hasta reto 03

--- assets/data.js
+++ assets/data.js
@@ -1,8 +1,8 @@
 
 export const data = [
     {
-        imageDesktop: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges/blob/master/01%20-%20fylo%20data%20storage%20component%20master/design/desktop-design.jpg?raw=true",
-        imageMobile: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges/blob/master/01%20-%20fylo%20data%20storage%20component%20master/design/mobile-design.jpg?raw=true",
+        imageDesktop: "./01 - fylo data storage component master/design/desktop-design.jpg",
+        imageMobile: "./01 - fylo data storage component master/design/mobile-design.jpg",
         title: "Fylo data storage component",
         description: "Este reto tiene algunos desafíos CSS muy interesantes en su diseño que pusieron en práctica mis habilidades y conocimientos en el tema",
         nivel: 1,
@@ -11,13 +11,33 @@ export const data = [
         tools: ["HTML", "CSS","FlexBox"]
     },
     {
-        imageDesktop: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges/blob/master/02%20-%20social%20proof%20section%20master/design/desktop-design.jpg?raw=true",
-        imageMobile: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges/blob/master/02%20-%20social%20proof%20section%20master/design/mobile-design.jpg?raw=true",
+        imageDesktop: "./02 - social proof section master/design/desktop-design.jpg",
+        imageMobile: "./02 - social proof section master/design/mobile-design.jpg",
         title: "Social proof section",
         description: "Este reto estuvo perfecto para practicar y mejorar mis habilidades en FlexBox y Web Responsive Design",
         nivel: 1,
         url: "./02 - social proof section master/index.html",
         code: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges/tree/master/02%20-%20social%20proof%20section%20master",
         tools: ["HTML", "CSS","FlexBox"]
+    },
+    {
+        imageDesktop: "./03 - single price grid component master/design/desktop-design.jpg",
+        imageMobile: "./03 - single price grid component master/design/mobile-design.jpg",
+        title: "Single price grid component",
+        description: "Decidí resolver este reto básico utilizando Sass como pre procesador de CSS (en este caso utilicé un archivo .scss)",
+        nivel: 1,
+        url: "./03 - single price grid component master/index.html",
+        code: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges/tree/master/03%20-%20single%20price%20grid%20component%20master",
+        tools: ["HTML", "CSS", "Sass", "FlexBox"]
+    },
+    {
+        imageDesktop: "",
+        imageMobile: "",
+        title: "",
+        description: "",
+        nivel: 1,
+        url: "",
+        code: "",
+        tools: []
     }
 ];
