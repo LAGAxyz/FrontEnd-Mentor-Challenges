fbc05814185e69fd217d758f9d7d2e15e8ccd0bd
LAGA <gutierrezanicamaluisalberto@hotmail.com>
2021-02-23 20:47:53 -0500

    Data agregada a la página principal de retos

--- assets/data.js
+++ assets/data.js
@@ -5,7 +5,7 @@ export const data = [
         imageMobile: "./01 - fylo data storage component master/design/mobile-design.jpg",
         title: "Fylo data storage component",
         description: "Este reto tiene algunos desafíos CSS muy interesantes en su diseño que pusieron en práctica mis habilidades y conocimientos en el tema",
-        nivel: 1,
+        nivel: 2,
         url: "./01 - fylo data storage component master/index.html",
         code: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges/tree/master/01%20-%20fylo%20data%20storage%20component%20master",
         tools: ["HTML", "CSS","FlexBox"]
@@ -38,6 +38,26 @@ export const data = [
         nivel: 1,
         url: "./04 - four card feature section master/index.html",
         code: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges/tree/master/04%20-%20four%20card%20feature%20section%20master",
+        tools: ["HTML", "CSS", "Sass", "FlexBox"]
+    },
+    {
+        imageDesktop: "./05 - chat app illustration master/design/desktop-design.jpg",
+        imageMobile: "./05 - chat app illustration master/design/mobile-design.jpg",
+        title: "Chat app CSS illustration",
+        description: "En este reto utilicé puro HTML 5 y CSS 3 para diseñar todo; esto incluye el fondo, los textos y el celular con todo su contenido.",
+        nivel: 3,
+        url: "./05 - chat app illustration master/index.html",
+        code: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges/tree/master/05%20-%20chat%20app%20illustration%20master",
+        tools: ["HTML", "CSS", "Sass", "FlexBox"]
+    },
+    {
+        imageDesktop: "./06 - ip address tracker master/design/desktop-design.jpg",
+        imageMobile: "./06 - ip address tracker master/design/mobile-design.jpg",
+        title: "IP Address Tracker",
+        description: "Este reto incluye el uso de la API de mapas 'Leaflet', así como también un contenedor flotante y un diseño adaptable a la pantalla.",
+        nivel: 3,
+        url: "./06 - ip address tracker master/index.html",
+        code: "https://github.com/LAGAxyz/FrontEnd-Mentor-Challenges/tree/master/06%20-%20ip%20address%20tracker%20master",
         tools: ["HTML", "CSS", "Sass", "FlexBox"]
     }
 ];
